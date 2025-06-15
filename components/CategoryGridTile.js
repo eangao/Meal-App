6be40da68a62004b57747979d65fb67825e50915
eangao <elmarcera@gmail.com>
2025-06-15 17:37:16 +0800

    Displaying Items in a Grid

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +19,38 @@ function CategoryGridTile({ title, color }) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    backgroundColor: "white",
+    overflow: Platform.OS === "android" ? "hidden" : "visible", // Ensures that the shadow and border radius are applied correctly
+  },
+  button: {
+    flex: 1,
+  },
+
+  buttonPressed: {
+    opacity: 0.5, // Changed from '0.25' to '0.5' for a more noticeable effect
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold", // Changed from 'bold' to '600' for better readability
+    color: "black",
+    fontSize: 18,
+  },
+});
